cb5d9c2e46a58f703b5a92e31b0fb37736faa5bf
Satoru Tanaka <ittimfn@gmail.com>
2020-09-14 17:36:54 +0900

    中断

---
+++ src/main/java/sample/spring/usemvc/controller/App.java
@@ -0,0 +1,11 @@
+package sample.spring.usemvc.controller;
+
+/**
+ * Hello world!
+ *
+ */
+public class App {
+    public static void main( String[] args ) {
+        System.out.println( "Hello World!" );
+    }
+}
